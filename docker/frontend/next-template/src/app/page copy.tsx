413b33c7a3499ac0b122928bb50708b362625960
BekxFR <104771705+BekxFR@users.noreply.github.com>
2023-07-05 15:10:45 +0200

    C02 modify docker part - add next and dev mode

--- docker/frontend/next-template/src/app/page copy.tsx
+++ docker/frontend/next-template/src/app/page copy.tsx
@@ -1,29 +1,29 @@
-'use client'
-
 // import Image from 'next/image'
 // import styles from './page.module.css'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Item {
 	id: number;
 	name: string;
-}
+  }
 
+// Add the "use client" comment to mark the parent component as a Client Component
+// use client
   const MyPage: React.FC = () => {
 	const [data, setData] = useState<Item[] | null>(null);
   
 	useEffect(() => {
 	  const fetchData = async () => {
 		try {
-		  const response = await fetch('http://localhost:3000/test');
+		  const response = await fetch('http://localhost:3000/api/data');
 		  const responseData = await response.json();
 		  setData(responseData);
-		  console.log(responseData);
 		} catch (error) {
 		  console.error('Erreur lors de la récupération des données du backend', error);
 		}
 	  };
-	fetchData();
+  
+	  fetchData();
 	}, []);
   
 	return (
